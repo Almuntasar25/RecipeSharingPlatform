480d859b85fe85689089c9f1754dce334e48deb2
almuntasar25 <almuntasar.x@gmail.com>
2025-06-22 13:43:14 +0400

    add Entitys and Configuration

---
+++ RecipeSharingPlatform/RecipePlatform.DAL/Configuration/RecipesConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace RecipePlatform.DAL.Configuration
+{
+    public class RecipesConfiguration 
+    {
+
+    }
+}
---
+++ RecipeSharingPlatform/RecipePlatform.DAL/Context/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace RecipePlatform.DAL.Context
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(assembly: typeof(ApplicationDbContext).Assembly);
+        }
+        public DbSet<Recipe> Recipes { get; set; }
+        public DbSet<User> Users { get; set; }
+
+
+
+    }
+}
